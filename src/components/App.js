d47989b3fd679ad1b312ba7e5c5cc40a6d30b75b
Shankar Jayaprakash <79354504+shankarjp@users.noreply.github.com>
2022-09-06 23:45:33 +0530

    fix(Routing): Added Nested Routing to report

--- src/components/App.js
+++ src/components/App.js
@@ -11,22 +11,25 @@ const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/test' element={<Test />} />
-                <Route path='/login' element={<Test />} />
-                <Route path='/register' element={<Test />} />
-                <Route path='/accomodation' element={<Test />} />
-                <Route path='/event-participation' element={<Test />} />
-                <Route path='/tshirt' element={<Test />} />
-                <Route path='/tours' element={<Test />} />
-                <Route path='/previews' element={<Test />} />
-                {/* <Route path='/report/registered' element={<Test />} />
-                <Route path='/report/summary' element={<Test />} />
-                <Route path='/report/tshirt' element={<Test />} />
-                <Route path='/report/participation' element={<Test />} />
-                <Route path='/report/totalcost' element={<Test />} />
-                <Route path='/report/paymentstatus' element={<Test />} />
-                <Route path='/report/balance' element={<Test />} /> */}
+                <Route path='' element={<Home />} />
+                <Route path='test' element={<Test />} />
+                <Route path='login' element={<Test />} />
+                <Route path='register' element={<Test />} />
+                <Route path='accomodation' element={<Test />} />
+                <Route path='event-participation' element={<Test />} />
+                <Route path='tshirt' element={<Test />} />
+                <Route path='tours' element={<Test />} />
+                <Route path='previews' element={<Test />} />
+                <Route path='/report'>
+                    <Route index element={<Test />} />
+                    <Route path='registered' element={<Test />} />
+                    <Route path='summary' element={<Test />} />
+                    <Route path='tshirt' element={<Test />} />
+                    <Route path='participation' element={<Test />} />
+                    <Route path='totalcost' element={<Test />} />
+                    <Route path='paymentstatus' element={<Test />} />
+                    <Route path='balance' element={<Test />} />
+                </Route>
                 <Route path='*' element={<NotFound />} />
             </Routes>
         </Router>
