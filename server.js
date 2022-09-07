1c203fd3e368a3b73bd1fbf11e707324918ad8bc
Shankar Jayaprakash <79354504+shankarjp@users.noreply.github.com>
2022-09-07 20:52:26 +0530

    fix: Basic Setup

--- server.js
+++ server.js
@@ -3,12 +3,22 @@ import apiRouter from './api';
 
 import express from 'express';
 const server = express();
-
+const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/test');
 
 server.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/test", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 server.get(['/',
 '/login',
