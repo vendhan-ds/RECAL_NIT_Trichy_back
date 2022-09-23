98c2387ae5f84aeb8a41b372ad8ed82877d53643
mharrish7 <harrishkumar100@gmail.com>
2022-09-23 17:20:31 +0530

    tshirts and tour partially done

--- src/components/pages/Tours.js
+++ src/components/pages/Tours.js
@@ -1,8 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Tours() {
+
+    const [tour,setn] = useState(false);
+
     return (
-        <h1>Tours</h1>
+        <>
+        <div className='mtitle'><h1>Tours</h1></div>
+        <div className='mainc'>
+        <label>I am Interested in Tour option</label>
+        <input type="checkbox" onChange={(e) => setn(e.target.checked)}></input>
+        <br />
+        <br />
+        {tour && <div className='rdetails'>
+        <table>
+            <thead>
+                <tr>
+                    <td>Tour</td>
+                    <td>Trichy Local</td>
+                    <td>Phuket-Krabi</td>
+                    <td>Mysore-Bandipur</td>
+                    <td>Belur-Hampi</td>
+                </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>From</td>
+                <td></td>
+                <td>26th Jan</td>
+                <td>26th Jan</td>
+                <td>29th Jan</td>
+            </tr> 
+            <tr>
+                <td>To</td>
+                <td></td>
+                <td>2nd Feb</td>
+                <td>29th Jan</td>
+                <td>1st Feb</td>
+            </tr>
+            <tr>
+                <td>Duration</td>
+                <td></td>
+                <td>5N/6D</td>
+                <td>2N/3D</td>
+                <td>3N/4D</td>
+            </tr>
+            <tr>
+                <td>Cost/head</td>
+                <td></td>
+                <td>53,000</td>
+                <td>20,000</td>
+                <td>15,000</td>
+            </tr>
+            </tbody>
+        </table>
+        </div>}
+        </div>
+        </>
     );
 }
 
--- src/components/pages/Tshirt.js
+++ src/components/pages/Tshirt.js
@@ -1,9 +1,68 @@
-import React from 'react';
+import { func, string } from 'prop-types';
+import React, { useState } from 'react';
 
 function Tshirt() {
+
+    const [need,setn] = useState(false);
+
+    function sendpost(){
+        console.log('clicked');
+    }
     return (
-        <h1>Tshirt</h1>
-    );
+        <> 
+        <div className='mtitle'><h1>Tshirt</h1></div>
+        <div className='mainc1'>
+            <label>I am Interested in T-Shirt</label>
+            <input type="checkbox" onChange={(e) => setn(e.target.checked)}></input>
+        {need && <div>
+            <table>
+                <tbody>
+                <h1>T-shirt for men</h1>
+                    <tr>
+                        <td>"T-Shirt (Dark Brick Red - Supima Cotton)
+Design - Polo with Collar"
+</td>
+                    <td><input type="checkbox"></input></td>
+                    </tr>
+                    <tr>
+                        <td>"T-Shirt (Navy Blue - Sweat-wicking Fabric)
+Design - Round Neck for Walking & Exercise)"
+</td>
+                    <td><input type="checkbox"></input></td>
+                    </tr>
+                    <h1>T-shirt for Women</h1>
+                    <tr>
+                        <td>T-Shirt Navy Blue - Sweat-wicking Fabric)
+Design - Round Neck for Walking & Exercise
+</td>
+                    <td><input type="checkbox"></input></td>
+                    </tr>
+                    <tr>
+                        <td>"T-Shirt (Royal Blue - Sweat-wicking Fabric)
+Design - Round Neck for Walking & Exercise)"
+</td>
+                    <td><input type="checkbox"></input></td>
+                    </tr>
+                    <h1>T-Shirt for GrandChildren</h1>
+                    <tr>
+                        <td>"T-Shirt (Maroon - Supima Cotton)
+Design - Round Neck"
+</td>
+                    <td><input type="checkbox"></input></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        }
+        
+        <br />
+        </div>
+        <div className='c1but'>
+        <button onClick={() => {window.location.href = "/event-participation"}} className="eventbut">Go Back and edit</button>
+        <button onClick={() => sendpost()} className="eventbut">Save and continue</button>
+        </div>        
+ 
+ </>   );
 }
 
 export default Tshirt;
